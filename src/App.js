b3e96c25ec79637c61531023e7898a5d2842fa24
Sandeep Sharma <blueskymail234@gmail.com>
2020-05-16 23:25:10 +0545

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker'
+
+
+import {Cards, Chart, CountryPicker} from './components';
+
+import styles from './App.module.css';
+
+import { fetchData } from './api';
+
+import image from './images/image.png';
+
+class App extends React.Component {
+
+    state = {
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData })
+    }
+
+    handleCountryChange =   async (country) => {
+        const fetchedData = await fetchData(country);
+
+        console.log(fetchData);
+
+        this.setState({ data: fetchedData, country: country });
+
+
+        //fetched the data
+
+
+
+
+        //set the data
+    }
+
+    render(){
+
+        const { data, country } = this.state;
+
+        return (
+        <div className={styles.container}>
+           <img className={styles.image} src ={image} alt="COVID-19" />
+           <Cards data={data} />
+           <CountryPicker handleCountryChange={this.handleCountryChange}/>
+           <Chart  data={data} country={country}/>
+           <h3>Developed by Sandeep Sharma</h3>
+
+        </div>
+        );
+            
+        
+    }
+
 }
 
 export default App;
